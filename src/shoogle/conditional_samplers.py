Fix flat-tail BPL model with JAX; Back to single-precision; Guess weight field automatically in run_shoogle

src/shoogle/conditional_samplers.py
@@ -41,7 +41,7 @@ class TemplateSampler(object):
     NUTS sampler, which provides efficient MCMC sampling.
     """
 
-    def __init__(self, proffile, weights, minsigma=0.001, maxsigma=0.25, maxwraps=2):
+    def __init__(self, proffile, weights, minsigma=0.001, maxsigma=0.5, maxwraps=2):
         """
         Initialises the object, including reading a starting estimate for
         the profile parameters from a file.
@@ -439,7 +439,7 @@ class EdepTemplateSampler(TemplateSampler):
     """
 
     def __init__(
-        self, proffile, weights, log10E, minsigma=0.001, maxsigma=0.25, maxwraps=2
+        self, proffile, weights, log10E, minsigma=0.001, maxsigma=0.5, maxwraps=2
     ):
         """
         Initialises the object, including reading a starting estimate for
@@ -514,6 +514,7 @@ class EdepTemplateSampler(TemplateSampler):
 
         tau = jnp.array(tau)
         phases = jnp.array(phases)
+        log10E = jnp.array(log10E)
 
         return self._jax_template(tau, phases, log10E)
 
@@ -1005,7 +1006,7 @@ class TimingModelSampler(object):
                          Upper-triangular Cholesky decomposition, U where C = U^T U
                          for the posterior covariance matrix C
         """
-        post_cov_inv = jnp.asarray(MT_Sigma_inv_M + inv_prior_cov, dtype=jnp.float64)
+        post_cov_inv = MT_Sigma_inv_M + inv_prior_cov
         post_cov_inv_U = cholesky(post_cov_inv, lower=False)
 
         theta_opt = cho_solve(
@@ -1122,7 +1123,7 @@ def bpl_flattail_powspec(pars, freqs):
                 Power-spectral density, in units of s^2 d
     """
 
-    bpl = self.bpl_powspec(pars, freqs)
+    bpl = bpl_powspec(pars, freqs)
     logkappa = pars[3]
 
     flat = 10 ** (2 * logkappa) * YR3_TO_S2D
@@ -1340,7 +1341,7 @@ class NoiseAndTimingModelSampler(TimingModelSampler):
                 jnp.where(
                     self.noise_freqs[c] > 0,
                     (
-                        bpl_powspec(all_hyp[c], self.noise_freqs[c])
+                        bpl_flattail_powspec(all_hyp[c], self.noise_freqs[c])
                         * self.min_freqs[c]
                         * INVYR_TO_INVDAY
                     ),

src/shoogle/gibbs_sampler.py
@@ -31,7 +31,7 @@ from shoogle.conditional_samplers import *
 
 import jax
 
-jax.config.update("jax_enable_x64", True)
+#jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 
 
@@ -67,7 +67,7 @@ class Gibbs(object):
         parfile=None,
         ft1file=None,
         ft2file=None,
-        weightfield="MODEL_WEIGHT",
+        weightfield=None,
         templatefile=None,
         wmin=0.05,
         timfile=None,
@@ -200,7 +200,7 @@ class Gibbs(object):
                 FC_free = False
                 GAM0 = None
                 GAM_free = False
-                KAPPA0 = None
+                KAPPA0 = -20.0
                 KAPPA_free = False
 
                 for line in lines:
@@ -225,33 +225,20 @@ class Gibbs(object):
                     if A_free or FC_free or GAM_free or KAPPA_free:
                         self.fit_TN = True
 
-                    if KAPPA0 is not None:
-                        self.noise_models.append(
-                            FlatTailBrokenPowerLaw(
-                                self.WXfreqs,
-                                self.WXSinds,
-                                self.WXCinds,
-                                np.array(
-                                    [A_free, FC_free, GAM_free, KAPPA_free], dtype=bool
-                                ),
-                                self.Tobs,
-                                np.array([logA0, logFC0, GAM0, KAPPA0]),
-                                prefix,
-                            )
+                    self.noise_models.append(
+                        FlatTailBrokenPowerLaw(
+                            self.WXfreqs,
+                            self.WXSinds,
+                            self.WXCinds,
+                            np.array(
+                                [A_free, FC_free, GAM_free, KAPPA_free], dtype=bool
+                            ),
+                            self.Tobs,
+                            np.array([logA0, logFC0, GAM0, KAPPA0]),
+                            prefix,
                         )
+                    )
 
-                    else:
-                        self.noise_models.append(
-                            BrokenPowerLaw(
-                                self.WXfreqs,
-                                self.WXSinds,
-                                self.WXCinds,
-                                np.array([A_free, FC_free, GAM_free], dtype=bool),
-                                self.Tobs,
-                                np.array([logA0, logFC0, GAM0]),
-                                prefix,
-                            )
-                        )
                     if A_free == 2:
                         self.noise_models[-1].linear_amp_prior = True
                     nc += 1

src/shoogle/plot_gibbs_results.py
@@ -1251,31 +1251,21 @@ class GibbsResults(object):
             phase_shifts = self.psr.M @ theta
             phases = self.psr.phi + phase_shifts - np.mean(phase_shifts)
 
-            gaussians = []
-            for p in range(self.psr.npeaks):
-                gaussians.append(
-                    LCGaussian(
-                        p=[
-                            self.template_chain[s, 2 * self.psr.npeaks + p],
-                            np.mod(self.template_chain[s, self.psr.npeaks + p], 1.0),
-                        ]
+            phi = np.arange(-0.005, 0.005, 0.00001)
+
+            logL = np.zeros_like(phi)
+
+            for idx in range(len(phi)):
+                shifted_phases = np.mod(phases + phi[idx], 1.0)
+
+                logL[idx] = np.sum(
+                    np.log(
+                        self.psr.w * self.psr.tau_sampler.template(self.template_chain[s,:],shifted_phases,self.psr.log10E)
+                        + (1 - self.psr.w)
                     )
                 )
-
-            template = LCTemplate(
-                gaussians, norms=self.template_chain[s, : self.psr.npeaks]
-            )
-
-            phi = np.arange(-0.005, 0.005, 0.00001)
-            shifted_phases = np.mod(phases[:, None] + phi[None, :], 1.0)
-            logL = np.sum(
-                np.log(
-                    self.psr.w[:, None] * template(shifted_phases)
-                    + (1 - self.psr.w[:, None])
-                ),
-                axis=0,
-            )
-
+                if idx % 10 == 0:
+                    print(idx, "/", len(phi), end = "\r")
             pdf = np.exp(logL - logL.max())
             pdf /= np.trapezoid(pdf, phi)
             dphi_sq = (