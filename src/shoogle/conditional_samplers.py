Check for NaNs in samples; Enforce symmetric covariance matrix; other fixes

src/shoogle/conditional_samplers.py
@@ -360,7 +360,7 @@ class TemplateSampler(object):
         self.kernel = kernel
         tau, logprior = self._samples_to_phys(state.position)
 
-        return np.array(tau), sample_key
+        return tau, sample_key
 
     def sample(self, tau, phases, key, num_samples=1000):
         """
@@ -979,8 +979,11 @@ class TimingModelSampler(object):
         resids = self.phi - mu_z
         precisions = jnp.where(sigma_z > 0, 1.0 / sigma_z**2, 0.0)
 
-        MT_Sigma_inv_M = jnp.einsum("ji,j,jk->ik", self.M, precisions, self.M)
-        MT_Sigma_inv_R = jnp.einsum("ji,j,j->i", self.M, precisions, resids)
+        MT_Sigma_inv_M = self.M.T @ (precisions[:,None] * self.M)
+        MT_Sigma_inv_R = self.M.T @ (precisions * resids)
+
+        # Enforce symmetry
+        MT_Sigma_inv_M = 0.5 * (MT_Sigma_inv_M + MT_Sigma_inv_M.T)
 
         return MT_Sigma_inv_M, MT_Sigma_inv_R
 
@@ -1210,7 +1213,6 @@ class NoiseAndTimingModelSampler(TimingModelSampler):
 
             if "OPV" in self.noise_models[c].prefix:
                 scales[c] = 1.0 / PB0.to_value("s") ** 2
-                print(scales)
 
             num_pars = len(self.noise_models[c].free)
             hyp0[c, :num_pars] = self.noise_models[c].x0

src/shoogle/gibbs_sampler.py
@@ -200,7 +200,7 @@ class Gibbs(object):
                 FC_free = False
                 GAM0 = None
                 GAM_free = False
-                KAPPA0 = -20.0
+                logKAPPA0 = -20.0
                 KAPPA_free = False
 
                 for line in lines:
@@ -216,7 +216,7 @@ class Gibbs(object):
                         GAM0 = float(line.split()[1])
                         GAM_free = int(line.split()[2])
                     if line.split()[0] == f"{prefix}_REDKAPPA":
-                        KAPPA0 = float(line.split()[1])
+                        logKAPPA0 = float(line.split()[1])
                         KAPPA_free = int(line.split()[2])
 
                 if logA0 is not None:
@@ -234,7 +234,7 @@ class Gibbs(object):
                                 [A_free, FC_free, GAM_free, KAPPA_free], dtype=bool
                             ),
                             self.Tobs,
-                            np.array([logA0, logFC0, GAM0, KAPPA0]),
+                            np.array([logA0, logFC0, GAM0, logKAPPA0]),
                             prefix,
                         )
                     )
@@ -261,6 +261,8 @@ class Gibbs(object):
                 A_free = False
                 FC_free = False
                 GAM_free = False
+                logKAPPA0 = -20.0
+                KAPPA_free = False
 
                 for line in lines:
                     if len(line.split()) < 2:
@@ -274,6 +276,9 @@ class Gibbs(object):
                     if line.split()[0] == f"{prefix}_REDGAM":
                         GAM0 = float(line.split()[1])
                         GAM_free = int(line.split()[2])
+                    if line.split()[0] == f"{prefix}_REDKAPPA":
+                        logKAPPA0 = float(line.split()[1])
+                        KAPPA_free = int(line.split()[2])
 
                 if logA0 is not None:
                     if not hasattr(self, "OPVfreqs"):
@@ -282,17 +287,20 @@ class Gibbs(object):
                         self.fit_OPV = True
 
                     self.noise_models.append(
-                        BrokenPowerLaw(
+                        FlatTailBrokenPowerLaw(
                             self.OPVfreqs,
                             self.OPVSinds,
                             self.OPVCinds,
-                            [A_free, FC_free, GAM_free],
+                            [A_free, FC_free, GAM_free, KAPPA_free],
                             self.Tobs,
-                            np.array([logA0, logFC0, GAM0]),
+                            np.array([logA0, logFC0, GAM0, logKAPPA0]),
                             prefix,
                         )
                     )
                     nc += 1
+
+                    if A_free == 2:
+                        self.noise_models[-1].linear_amp_prior = True
 
                 else:
                     break
@@ -897,6 +905,19 @@ class Gibbs(object):
         print(jax.devices())
 
         # template alignment check
+        logL_max = 0
+        for dphi in np.arange(-0.5,0.5,0.001):
+            tau = np.array(self.tau_sampler.tau_0)
+            tau[self.npeaks:2 * self.npeaks] += dphi
+            if self.Edep:
+                tau[4 * self.npeaks:5 * self.npeaks] += dphi
+            logL = self.tau_sampler._log_like(jnp.array(tau),jnp.array(self.phi))
+            if logL > logL_max:
+                logL_max = logL
+                print(dphi, logL)
+                tau_opt = jnp.array(tau)
+        self.tau_sampler.tau_0 = tau_opt
+
         fig, ax = plt.subplots(2, 1, sharex=True, height_ratios=[1, 2], figsize=(5, 10))
         i = np.argsort(self.w)
         p = np.arange(0, 1, 0.001)
@@ -994,20 +1015,22 @@ class Gibbs(object):
         if not hasattr(self.tau_sampler, "kernel"):
             print("Setting up template sampler")
             tau, key = self.tau_sampler.setup_sampler(jphi - phase_shifts)
+            print()
+
+        if key is None:
+            key = jax.random.key(0)
+        mu_z, sigma_z, key = self.zm_sampler.sample_z_given_theta_tau(
+            tau, jphi - phase_shifts, key
+        )
 
         if self.nhyp > 0:
             self.timing_sampler.hyp_0 = jnp.array(hyp)
             if not hasattr(self.timing_sampler, "kernel"):
                 print("Setting up hyper-parameter sampler")
-                mu_z, sigma_z, key = self.zm_sampler.sample_z_given_theta_tau(
-                    tau, jphi - phase_shifts, key
-                )
                 hyp, key = self.timing_sampler.setup_sampler(mu_z, sigma_z)
-
+                print()
         c = 0
 
-        if key is None:
-            key = jax.random.key(0)
         keys = jax.random.split(key, 10000)
 
         if self.nhyp > 0:
@@ -1059,12 +1082,16 @@ class Gibbs(object):
 
                 return (phase_shifts, tau), (tau, theta)
 
+        print("JIT-compiling the Gibbs sampler")
+        state, samples = gibbs_sampling_loop(state,key)
+
         progress = tqdm(
             total=n_acor_target * update, desc="Gibbs sampling", smoothing=0.0
         )
         start_time = time.time()
         progress.start_t = start_time
         progress.last_print_t = start_time
+
 
         while (c < update or n_acor < n_acor_target) and (c < max_iterations):
 
@@ -1100,8 +1127,13 @@ class Gibbs(object):
             if self.nhyp > 0:
                 hyp = samples[1]
                 theta = samples[2]
+                if np.any(np.isnan(hyp))
+                    raise ValueError("Error: found a NaN in the samples")
             else:
                 theta = samples[1]
+
+            if np.any(np.isnan(theta)) or np.any(np.isnan(tau)):
+                raise ValueError("Error: found a NaN in the samples")
 
             self.timing_chain[c] = theta[: self.n_timing_pars]
             self.template_chain[c] = tau

src/shoogle/noise_models.py
@@ -50,6 +50,7 @@ class NoiseModel(object):
             raise ValueError("Incorrect number of hyperparameters specified")
 
         self.prefix = prefix
+        self.linear_amp_prior = False
 
         return
 
@@ -179,13 +180,12 @@ class BrokenPowerLaw(NoiseModel):
                 -np.log10(self.Tobs * (u.d / u.yr)) - 1,
                 -np.log10(self.Tobs * (u.d / u.yr)) + 2,
             ],
-            [1, 7],
+            [1, 9],
         ]
 
         if "OPV" in self.prefix:
             self.bounds[0] = [-15, -5]
-            self.bounds[2] = [0.0, 10.0]
-
+            self.bounds[2] = [1.0, 9.0]
 
     def cov(self, t, pars):
 
@@ -265,9 +265,14 @@ class FlatTailBrokenPowerLaw(NoiseModel):
                 -np.log10(self.Tobs * (u.d / u.yr)) - 1,
                 -np.log10(self.Tobs * (u.d / u.yr)) + 2,
             ],
-            [1, 7],
+            [1, 9],
             [-20, -9],
         ]
+
+        if "OPV" in self.prefix:
+            self.bounds[0] = [-15, -5]
+            self.bounds[2] = [1.0, 9.0]
+
 
     """
     def cov(self, t, pars):