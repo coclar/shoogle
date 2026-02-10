Fixing error in linear amplitude prior weighting factor

src/shoogle/conditional_samplers.py
@@ -1316,7 +1316,7 @@ class NoiseAndTimingModelSampler(TimingModelSampler):
         amp_prior = 0.0
         for c in range(len(x)):
             amp_prior += jax.lax.cond(
-                self.linear_priors[c], lambda: hyp[c], lambda: 0.0
+                self.linear_priors[c], lambda: hyp[c] * np.log(10), lambda: 0.0
             )
 
         return hyp, logprior + amp_prior

src/shoogle/noise_models.py
@@ -210,7 +210,7 @@ class BrokenPowerLaw(NoiseModel):
     def log_prior(self, pars):
 
         if self.linear_amp_prior:
-            return pars[0]
+            return pars[0] * np.log(10)
 
         return 0.0
 
@@ -294,7 +294,7 @@ class FlatTailBrokenPowerLaw(NoiseModel):
     def log_prior(self, pars):
 
         if self.linear_amp_prior:
-            return pars[0]
+            return pars[0] * np.log(10)
 
         return 0.0
 